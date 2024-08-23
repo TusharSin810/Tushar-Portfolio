d91ac23aca7409cdcb45f030010cc9fad34696e5
TusharSin810 <tsinghal666@gmail.com>
2024-08-23 22:37:17 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-violet-700 bg-rose-800 font-extrabold text-2xl p-3'>Hello There</h1>
+    </div>    
+  )
+}
+
+export default App
