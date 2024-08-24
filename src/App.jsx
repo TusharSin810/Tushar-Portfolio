c2e127949a6b21d27ae8392c7cba008024e8d2f7
TusharSin810 <tsinghal666@gmail.com>
2024-08-24 19:48:28 +0530

    landing Page Div Setup

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ function App() {
       <div className='flex items-center justify-center min-h-96'>
         <h1 className='text-2xl font-extrabold'>Hello There</h1>
       </div>
+      <div className='min-h-96'>
+        <p className='flex justify-center'>Hello my name is Tushar Singhal i am a web developer </p>
+      </div>
+           
     </div>
   )
 }
--- src/components/Particles.jsx
+++ src/components/Particles.jsx
@@ -1,25 +1,16 @@
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { useEffect, useMemo, useState } from "react";
-// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
-
+import { loadSlim } from "@tsparticles/slim";
 
 
 const ParticlesComponent = (props) => {
 
   const [init, setInit] = useState(false);
-  // this should be run only once per application lifetime
+  
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      //await loadAll(engine);
-      //await loadFull(engine);
       await loadSlim(engine);
-      //await loadBasic(engine);
+
     }).then(() => {
       setInit(true);
     });
