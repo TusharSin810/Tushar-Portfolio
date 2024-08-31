7a55549e02074ced415569f194981e4327274ec3
TusharSin810 <tsinghal666@gmail.com>
2024-08-31 19:24:28 +0530

    Media Queries

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <nav className='flex flex-wrap justify-end py-2 mx-4'>
         <ul className='mx-2 font-bold'>Projects</ul>
-        <ul className='mx-2 font-bold'>Experience</ul>
+        <ul className='mx-2 font-bold '>Experience</ul>
         <ul className='mx-2 font-bold'>Education</ul>
         <ul className='mx-2 font-bold'>Contact Me</ul>
       </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '375',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
         transitionDuration: {
           '2000': '2000ms',
