45752c0a9cb30552a243a33a90a1118a6814cb5f
TusharSin810 <tsinghal666@gmail.com>
2024-09-05 18:32:26 +0530

    navbar

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Navbar from './Navbar'
 import {useTypewriter, Cursor} from 'react-simple-typewriter'
 import { SocialIcon } from 'react-social-icons'
+import Navbar from './Navbar'
 
 
 
@@ -18,16 +18,16 @@ function Homepage() {
     <div>
         <Navbar />       
           <div className='flex flex-col h-svh m-6 my-10'>
-            <h1 className='text-3xl font-extrabold'>Hi ,<br/>I'm Tushar Singhal</h1>
+            <h1 className='text-3xl font-extrabold'>Hi,<br/>I'm Tushar Singhal</h1>
             <h2 className='my-6 font-bold text-xl'>I Am Into {" "}  
               <span className='text-[#4D0011]'>{typeEffect}</span>
               <span className='text-[#4B443C] font-bold'><Cursor/></span>
             </h2>
             <div className='flex justify-start gap-4 my-1'>
-              <SocialIcon url='https://x.com/TusharS91319723' target='_blank' />
-              <SocialIcon url='https://github.com/TusharSin810' target='_blank' />
-              <SocialIcon url='https://www.linkedin.com/in/tushar-singhal-921a41259/' target='_blank'/>
-              <SocialIcon url='mailto:tsinghal666@gmail.com' target='_blank' bgColor='#4D0011'/>
+              <SocialIcon url='https://x.com/TusharS91319723' target='_blank' className='hover:opacity-85' />
+              <SocialIcon url='https://github.com/TusharSin810' target='_blank' className='hover:opacity-85'/>
+              <SocialIcon url='https://www.linkedin.com/in/tushar-singhal-921a41259/' target='_blank' className='hover:opacity-85'/>
+              <SocialIcon url='mailto:tsinghal666@gmail.com' target='_blank' bgColor='#4D0011'className='hover:opacity-85'/>
             </div>
           </div>
           <div className=''>
