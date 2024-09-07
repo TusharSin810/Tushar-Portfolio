f01b05d282ba5594a570d8e9cf839e822b6a4f81
TusharSin810 <tsinghal666@gmail.com>
2024-09-07 22:49:23 +0530

    Changes to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,10 @@ function About() {
         <div className='h-max bg-white pb-8'>
             <h2 className='text-2xl font-extrabold flex w-screen justify-center p-5 pt-10'>About Me</h2>
             <div className='flex flex-col items-center'>
-                <p className='p-6 sm:hidden'>
-                    I’m an Electronics and Communication Engineering student at JIIT, Noida, with a passion for full-stack development. I build scalable, user-centric web applications and am currently expanding my skills in Web3, Data Science, and Machine Learning. I’m also pursuing a distance BSc in Data Science and Programming from IIT Madras.
+                <p className='p-6 flex sm:hidden'>
+                    I’m an Electronics and Communication Engineering student at JIIT, Noida, with a passion for full-stack development. I build scalable, user-centric web applications and am currently expanding my skills in Web3, Data Science, and Machine Learning.
+                    <br/>
+                    I’m also pursuing a distance BSc in Data Science and Programming from IIT Madras.
                 </p>
                 <p className='hidden p-4 sm:flex sm:w-3/5'>
                     I’m an undergraduate student pursuing a B.Tech in Electronics and Communication Engineering at Jaypee Institute of Information Technology (JIIT), Noida. Passionate about coding and full-stack development, I enjoy building dynamic, scalable web applications with a focus on innovation and performance.
