049c34114cb03b8ed92f7e28c39e36d65e99c53d
TusharSin810 <tsinghal666@gmail.com>
2024-09-09 20:26:12 +0530

    Changes to skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,45 +14,109 @@ function Skills() {
 
             <div class="group w-fit">
                 <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
-                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="HTML" loading="lazy"/>
-                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">HTML</p>
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="CSS" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">CSS</p>
                 </div>
             </div>
 
             <div class="group w-fit">
                 <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
-                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="HTML" loading="lazy"/>
-                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">HTML</p>
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="JavaScript" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">JavaScript</p>
                 </div>
             </div>
 
             <div class="group w-fit">
                 <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
-                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="HTML" loading="lazy"/>
-                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">HTML</p>
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="React Js" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">React Js</p>
                 </div>
             </div>
 
             <div class="group w-fit">
                 <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
-                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="HTML" loading="lazy"/>
-                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">HTML</p>
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Node Js" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Node Js</p>
                 </div>
             </div>
 
             <div class="group w-fit">
                 <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
-                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="HTML" loading="lazy"/>
-                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">HTML</p>
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Express" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Express</p>
                 </div>
             </div>
 
             <div class="group w-fit">
                 <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
-                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="HTML" loading="lazy"/>
-                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">HTML</p>
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="PostgreSQL" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Postgre SQL</p>
                 </div>
             </div>
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Tailwind CSS" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Tailwind CSS</p>
+                </div>
+            </div>
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="C++" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">C++</p>
+                </div>
+            </div>
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Python" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Python</p>
+                </div>
+            </div>    
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="GitHub" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">GitHub</p>
+                </div>
+            </div>
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Postman" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Postman</p>
+                </div>
+            </div>  
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Solidity" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Solidity</p>
+                </div>
+            </div>
+                       
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Machine Learning" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Machine Learning</p>
+                </div>
+            </div>
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Data Science" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Data Science</p>
+                </div>
+            </div>               
+
+
+            <div class="group w-fit">
+                <div class="flex items-center justify-center rounded-md h-[3rem] w-[3rem] xs:h-[4rem] xs:w-[4rem] sm:h-[5rem] sm:w-[5rem] p-2 sm:p-3 overflow-hidden bg-darkest border dark:border-white/[0.2] group-hover:border-lightest group-hover:border-opacity-50 relative z-20">
+                <img class="group-hover:hidden select-none" draggable="false" src="./skills/HTML5.svg" alt="Git" loading="lazy"/>
+                <p class="text-white select-none text-[10px] sm:text-sm font-medium text-center hidden group-hover:block">Git</p>
+                </div>
+            </div> 
 
         </div>
 
