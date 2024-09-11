161d5753438757e0bd2a4622fb029666aee2855a
TusharSin810 <tsinghal666@gmail.com>
2024-09-11 21:17:07 +0530

    Added card to projects tab

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+
 
 function Projects() {
   return (
     <div className='h-screen bg-[#4B443C]'>
-      <p>These are my projects !!</p>
+       <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
     </div>
   )
 }
