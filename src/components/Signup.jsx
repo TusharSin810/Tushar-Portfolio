8c6a23b0becd37b4c04ed08e1b8ec3a23bd1b9d7
TusharSin810 <tsinghal666@gmail.com>
2024-09-12 19:29:12 +0530

    Added background effect on login page

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import ParticlesSignup from './ParticlesSignup'
 function Signup() {
   return (
     <div>
+        <ParticlesSignup id="psignup" />
       <h1 className='text-black font-extrabold text-2xl w-screen flex justify-center p-10'>Sign-Up</h1>
     </div>
   )
