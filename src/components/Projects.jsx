3cfc2f0957e4530c45a7ff86cc19dc85ca46ab88
TusharSin810 <tsinghal666@gmail.com>
2024-09-23 21:41:36 +0530

    Created a General Card component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { CardCom } from './uiComponents/CardCom'
-
+import {CardCom} from './uiComponents/CardCom'
+import cardD from '../../data/cardinfo.json'
 
 function Projects() {
   return (
     <div className='h-max bg-[#4B443C] pt-5'>
       <h2 className='w-screen text-2xl font-extrabold text-white flex justify-center pb-4' >Projects</h2>
       <div className='flex flex-row justify-evenly'>
-        <CardCom />
-        <CardCom />
-        <CardCom />
+        {cardD.map((data, index) => (
+        <CardCom title={data.title} description={data.description} link={data.link} />
+      ))}
       </div>
     </div>
   )
--- src/components/uiComponents/CardCom.jsx
+++ src/components/uiComponents/CardCom.jsx
@@ -1,9 +1,10 @@
 "use client";
 
 import React from "react";
-import { CardBody, CardContainer, CardItem } from "../ui/3d-card";
+import { CardBody, CardContainer, CardItem } from "../ui/3d-card"
 
-export function CardCom() {
+
+export function CardCom({title,description,link}) {
   return (
     <CardContainer className="inter-var">
       <CardBody
@@ -13,14 +14,14 @@ export function CardCom() {
           translateZ="50"
           className="text-xl font-bold text-neutral-600 dark:text-white"
         >
-          Make things float in air
+          {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
         >
-          Hover over this card to unleash the power of CSS perspective
+          {description}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <img
@@ -34,19 +35,16 @@ export function CardCom() {
         <div className="flex justify-between items-center mt-8">
           <CardItem
             translateZ={20}
-            as="a"
-            href="https://twitter.com/mannupaaji"
-            target="__blank"
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white cursor-pointer"
           >
-            Try now →
+            <a href={link} target="_blank">Live</a>
           </CardItem>
           <CardItem
             translateZ={20}
             as="button"
             className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
           >
-            Sign up
+            <a href={link} target="_blank">Code</a>
           </CardItem>
         </div>
       </CardBody>
