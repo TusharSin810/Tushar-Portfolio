cccbd07115f507674ac3990cd7dcbc8571c76c6f
TusharSin810 <tsinghal666@gmail.com>
2024-09-24 17:44:50 +0530

    Header Modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,17 +20,17 @@ function Header() {
   }
   
   return (
-        <div className='flex flex-col h-svh m-6 my-10 sm:flex-row'>
+        <div className='flex flex-col h-svh m-6 my-10 sm:flex-row sm:justify-around'>
             <div className='mt-4'>
-              <h1 className='text-3xl font-extrabold'>Hi,<br/>I'm Tushar Singhal</h1>
-              <h2 className='my-6 font-bold text-xl'>I Am Into {" "}  
+              <h1 className='text-3xl font-extrabold sm:text-6xl'>Hi,<br/>I'm Tushar Singhal</h1>
+              <h2 className='my-6 font-bold text-xl sm:text-3xl'>I Am Into {" "}  
                 <span className='text-[#4D0011]'>{typeEffect}</span>
                 <span className='text-[#4B443C] font-bold'><Cursor/></span>
               </h2>
-              <button className='bg-[#4B443C] py-1 px-2 m-2 mb-10 rounded-xl font-semibold text-[#E4E5E8]'>About Me</button>
-              <button className='bg-[#4B443C] py-1 px-2 m-2 mb-10 rounded-xl font-semibold text-[#E4E5E8]'><a href='https://drive.google.com/file/d/1_K89DMcUKlSV5_g0w5ZdHqvFYzPd7Zt_/view?usp=sharing' target='_blank'>Resume</a></button>
-              <button className='bg-[#4B443C] py-1 px-2 m-2 mb-10 rounded-xl font-semibold text-[#E4E5E8]' onClick={chatpage}>Chat With Me </button>
-              <div className='flex justify-start gap-4 my-1'>
+                <button className=' hidden sm:inline-block bg-[#4B443C] py-1 px-2 m-2 rounded-xl font-semibold text-[#E4E5E8]'>About Me</button>
+                <button className='bg-[#4B443C] py-1 px-2 m-2 rounded-xl font-semibold text-[#E4E5E8]'><a href='https://drive.google.com/file/d/1_K89DMcUKlSV5_g0w5ZdHqvFYzPd7Zt_/view?usp=sharing' target='_blank'>Resume</a></button>
+                <button className='bg-[#4B443C] py-1 px-2 m-2 mb-10 rounded-xl font-semibold text-[#E4E5E8]' onClick={chatpage}>Chat With Me </button>
+              <div className='flex justify-start gap-4 my-1 sm:justify-start sm:gap-10'>
                 <SocialIcon url='https://x.com/TusharS91319723' target='_blank' className='hover:opacity-85' />
                 <SocialIcon url='https://github.com/TusharSin810' target='_blank' className='hover:opacity-85'/>
                 <SocialIcon url='https://www.linkedin.com/in/tushar-singhal-921a41259/' target='_blank' className='hover:opacity-85'/>
@@ -38,7 +38,7 @@ function Header() {
               <SocialIcon url='tushar_singhal' target='_blank' className='hover:opacity-85'/> 
               </div>
             </div>
-            <motion.img src="/images/Avatar.png" alt="Avatar image" className='h-auto w-auto max-w-96  max-h-96 mt-10 rounded-full'
+            <motion.img src="/images/Avatar.png" alt="Avatar image" className='h-auto w-auto max-w-96  max-h-96 mt-10 rounded-full sm:mt-6'
             whileHover={{
               scale:1.1
             }}
