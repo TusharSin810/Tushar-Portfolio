935bb039a9609bc9ceac8b818e7b08ad302a4e4f
TusharSin810 <tsinghal666@gmail.com>
2024-09-24 22:34:57 +0530

    Chat with me Page

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 function ChatPage() {
   onload
   return (
-    <div>
-       <h1>Chat With Me !!!</h1> 
+    <div className='bg-[#53565A] w-screen h-screen'>
+      <div className='w-screen h-screen flex justify-center items-end'>
+        <div className='flex gap-4 mb-10 p-6 px-40 rounded-full bg-[#E4E5E8]'>
+          <input type='text' placeholder='Type Your Message' className='rounded-full px-10 p-1'/>
+          <button className='bg-[#4D0011] p-2 px-6 rounded-full text-[#E4E5E8]'>Send</button>
+        </div>
+      </div> 
     </div>
   )
 }
