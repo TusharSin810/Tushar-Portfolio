a94b6262623ff1b9399f09f0aa883ad6834f9fdb
TusharSin810 <tsinghal666@gmail.com>
2024-09-24 18:55:05 +0530

    Added Card details

--- src/components/uiComponents/CardCom.jsx
+++ src/components/uiComponents/CardCom.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card"
 
 
-export function CardCom({title,description,link}) {
+export function CardCom({title,description,link,image}) {
   return (
     <CardContainer className="inter-var">
       <CardBody
@@ -25,7 +25,7 @@ export function CardCom({title,description,link}) {
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <img
-            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={image}
             height="1000"
             width="1000"
             className="h-50 w-full object-cover rounded-xl group-hover/card:shadow-xl"
