eb68434e28bfe971c20220e5647be78259c26d47
TusharSin810 <tsinghal666@gmail.com>
2024-09-28 08:33:03 +0530

    Chat Page Modified

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -3,13 +3,15 @@ import React from 'react'
 function ChatPage() {
   onload
   return (
-    <div className='bg-[#53565A] w-screen h-screen'>
-      <div className='w-screen h-screen flex justify-center items-end'>
-        <div className='flex gap-4 mb-10 p-6 px-40 rounded-full bg-[#E4E5E8]'>
-          <input type='text' placeholder='Type Your Message' className='rounded-full px-10 p-1'/>
-          <button className='bg-[#4D0011] p-2 px-6 rounded-full text-[#E4E5E8]'>Send</button>
+    <div className='flex flex-col h-screen items-center justify-end bg-[#53565A] pt-10 gap-2'>
+        <div className='bg-[#E4E5E8] h-96 w-4/5 flex justify-between rounded-3xl'>
         </div>
-      </div> 
+        <div>
+          <div className='flex gap-4 mb-10 p-6 px-72 rounded-full bg-[#E4E5E8]'>
+            <input type='text' placeholder='Type Your Message' className='rounded-full px-10 p-1'/>
+            <button className='bg-[#4D0011] p-2 px-6 rounded-full text-[#E4E5E8]'>Send</button>
+          </div>
+        </div> 
     </div>
   )
 }
