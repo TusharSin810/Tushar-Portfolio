babe96ebb96d465ce15b8e0120f470a19023b0a7
TusharSin810 <tsinghal666@gmail.com>
2024-09-30 16:46:57 +0530

    Education Section Added

---
+++ src/components/Education.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Education() {
+  return (
+    <div className='h-screen w-screen bg-[#4D0011]'>
+       Hello There 
+    </div>
+  )
+}
+
+export default Education
