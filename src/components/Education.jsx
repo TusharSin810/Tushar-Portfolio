ef91222f01054edafa5a761cdfda36d9d4df7183
TusharSin810 <tsinghal666@gmail.com>
2024-10-01 20:55:40 +0530

    Education Cards updated with data

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Meteorscard } from './uiComponents/Meteorscard'
-
+import eCard from '../../data/educationCard.json'
 function Education() {
   return (
     <div className='bg-[#E4E5E8] h-screen w-screen flex flex-col items-center p-8 gap-10'>
        <h2 className='font-extrabold text-3xl text-[#4D0011]'>Education</h2>
        <div className='flex gap-6'>
-      <Meteorscard />
-      <Meteorscard />
-      <Meteorscard />
+        {eCard.map((data, index) => (
+          <Meteorscard key={data.id || index} title={data.title} description={data.description} image={data.image} year={data.year}/>
+        ))}
        </div>
     </div>
    
--- src/components/uiComponents/Meteorscard.jsx
+++ src/components/uiComponents/Meteorscard.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { Meteors } from "../ui/meteors";
 
-export function Meteorscard() {
+export function Meteorscard({title,description,image,year}) {
   return (
     (<div className="">
       <div className=" w-full relative max-w-xs">
         <div
           className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
         <div
-          className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
-          <img src="/images/Avatar.png" alt="Avatar" className="h-40 w-full"/>
+          className="relative shadow-xl bg-[#53565A] border border-gray-800  px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
+          <img src={image} alt="Avatar" className="h-40 w-full"/>
           <h1 className="font-bold text-xl text-white mb-4 relative z-50 mt-2">
-            Meteors because they&apos;re cool
+            {title}
           </h1>
 
-          <p className="font-normal text-base text-slate-500 mb-4 relative z-50">
-            I don&apos;t know what to write so I&apos;ll just paste something
-            cool here. One more sentence because lorem ipsum is just
-            unacceptable. Won&apos;t ChatGPT the shit out of this.
+          <p className="font-normal text-base text-[#E4E5E8] relative z-50">
+            {description} <br/><br/>
+            {year}
           </p>
-           <Meteors number={20} />
+           <Meteors number={30} />
         </div>
       </div>
     </div>)
