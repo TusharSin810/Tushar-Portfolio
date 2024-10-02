7232d5f5567abed50e0c4d56f94fab1cb384ddc6
TusharSin810 <tsinghal666@gmail.com>
2024-10-02 21:08:23 +0530

    Created contact me Section

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 function ContactMe() {
   return (
-    <div className='h-screen w-screen bg-white'>
-        Contact ME 
+    <div className='h-screen w-screen bg-[#53565A] flex flex-col'>
+    <div className='pt-8 px-8 pb-4 font-extrabold text-3xl text-[#4D0011]'>
+        <h2 class="heading"><i class="fas fa-headset"></i> Get in <span>Touch</span></h2> 
+    </div>
+    <p className='w-1/2 text-lg text-[#E4E5E8] font-semibold px-8'>Feel free to get in touch with me. I am always open to discussing new projects, creative ideas or opportunities to be part of your visions.</p>
     </div>
   )
 }
