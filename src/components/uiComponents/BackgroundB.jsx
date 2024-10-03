0866094b4ec108e9fc652402007cc66bb84c6746
TusharSin810 <tsinghal666@gmail.com>
2024-10-03 22:16:33 +0530

    Contact Me Page Modified

--- src/components/uiComponents/BackgroundB.jsx
+++ src/components/uiComponents/BackgroundB.jsx
@@ -2,11 +2,12 @@
 import React from "react";
 import { BackgroundBeams } from "../ui/background-beams";
 import { SocialIcon } from 'react-social-icons'
+import { MovingBorder } from "./MovingBorder";
 
 export function BackgroundB() {
   return (
     (<div
-      className="h-screen w-full rounded-md bg-[#53565A] relative flex flex-col items-center antialiased pt-20">
+      className="h-screen w-full rounded-md bg-[#53565A] relative flex flex-col items-center antialiased pt-20 gap-2">
       <div className="max-w-2xl mx-auto p-4">
         <h1
           className="relative z-10 text-4xl md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-[#E4E5E8] to-neutral-500  text-center font-sans font-bold">
@@ -24,9 +25,12 @@ Feel free to get in touch with me. I am always open to discussing new projects, 
             <SocialIcon url='mailto:tsinghal666@gmail.com' target='_blank' bgColor='#4D0011'className='hover:opacity-85'/>
           <SocialIcon url='tushar_singhal' target='_blank' className='hover:opacity-85'/> 
         </div>
-        <div>
-          <h3>tsinghal666@gmail.com</h3>
-          <h3>9999999999</h3>
+        <div className="flex flex-col pt-4 items-center gap-10">
+          <MovingBorder />
+          <div className="flex w-screen justify-evenly">
+            <h3>tsinghal666@gmail.com</h3>
+            <h3>+91-8826126823</h3>
+          </div>
         </div>
       <BackgroundBeams />
     </div>)
