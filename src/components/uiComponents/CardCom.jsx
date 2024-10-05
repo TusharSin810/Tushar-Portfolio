31ff8c1836d4ede43be7a120a6a250d8bdc3bf35
TusharSin810 <tsinghal666@gmail.com>
2024-10-05 15:26:43 +0530

    UI changes in Projects Section

--- src/components/uiComponents/CardCom.jsx
+++ src/components/uiComponents/CardCom.jsx
@@ -8,18 +8,18 @@ export function CardCom({title,description,live,code,image}) {
   return (
     <CardContainer className="inter-var">
       <CardBody
-        className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-auto rounded-xl p-6 border"
+        className="bg-[#53565A] relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-auto rounded-xl p-6 border"
       >
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-neutral-600 dark:text-white"
+          className="text-xl font-bold text-white"
         >
           {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
-          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+          className="text-sm max-w-sm mt-2 text-neutral-300"
         >
           {description}
         </CardItem>
@@ -35,7 +35,7 @@ export function CardCom({title,description,live,code,image}) {
         <div className="flex justify-between items-center mt-8">
           <CardItem
             translateZ={20}
-            className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white cursor-pointer"
+            className="px-4 py-2 rounded-xl text-xs font-normal text-white cursor-pointer"
           >
             <a href={live} target="_blank">Live</a>
           </CardItem>
