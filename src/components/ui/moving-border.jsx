d14bb747132994461d03dc5c4f6768a48c7d732c
TusharSin810 <tsinghal666@gmail.com>
2024-10-08 19:33:09 +0530

    Ui Changes And More projects added

--- src/components/ui/moving-border.jsx
+++ src/components/ui/moving-border.jsx
@@ -36,7 +36,7 @@ export function Button({
         <MovingBorder duration={duration} rx="30%" ry="30%">
           <div
             className={cn(
-              "h-20 w-20 opacity-[1] bg-[radial-gradient(var(--rose-900)_40%,transparent_60%)]",
+              "h-14 w-24 opacity-[1] bg-[radial-gradient(var(--sky-700)_100%,transparent_80%)]",
               borderClassName
             )} />
         </MovingBorder>
