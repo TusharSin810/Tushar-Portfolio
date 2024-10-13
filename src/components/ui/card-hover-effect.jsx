a6657c8af2309f76c884bd58f42ca9ab16c4e5cc
TusharSin810 <tsinghal666@gmail.com>
2024-10-13 20:02:13 +0530

    Skills Page Updated

--- src/components/ui/card-hover-effect.jsx
+++ src/components/ui/card-hover-effect.jsx
@@ -24,7 +24,7 @@ export const HoverEffect = ({ items, className }) => {
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-2 h-[100px] w-[100px] sm:h-[130px] sm:w-[130px] bg-[#4D0011] bg-opacity-85 dark:bg-slate-800/[0.8] rounded-2xl"
+                className="absolute inset-2 h-[100px] w-[100px] sm:h-[130px] sm:w-[130px] bg-[#4D0011]/80 bg-opacity-85 dark:bg-slate-800/[0.8] rounded-2xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
