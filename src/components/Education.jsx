a861eee050799067c3243fd1984b4a417b238e81
TusharSin810 <tsinghal666@gmail.com>
2024-10-20 19:38:22 +0530

    Changes in Education Section

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import { Meteorscard } from './uiComponents/Meteorscard'
 import eCard from '../../data/educationCard.json'
 import ScrollAnimation from './uiComponents/ScrollAnimation'
 import { CardCom } from './uiComponents/CardCom'
 function Education() {
   return (
-    <div className='bg-white h-screen w-screen flex flex-col items-center p-8 gap-10'>
+    <div className='bg-white h-screen w-screen flex flex-col pt-10 px-10 gap-10'>
       <ScrollAnimation>
-       <h2 className='font-extrabold text-3xl text-[#4D0011]'>Education</h2>
-       <div className='flex gap-6'>
+       <h2 className='font-extrabold text-6xl text-[#4D0011] pb-4'>Education</h2>
+       <div className='flex w-full justify-between gap-2'>
         {eCard.map((data, index) => (
-          <Meteorscard key={data.id || index} title={data.title} description={data.description} image={data.image} year={data.year}/>
+          <CardCom key={data.id || index} title={data.title} description={data.description} image={data.image} year={data.year}/>
         ))}
        </div>
-       <CardCom />
-       <CardCom />
       </ScrollAnimation>
     </div>
    
--- src/components/uiComponents/CardCom.jsx
+++ src/components/uiComponents/CardCom.jsx
@@ -4,15 +4,15 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "../ui/3d-card"
 
 
-export function CardCom({title,description,live,code,image}) {
+export function CardCom({title,description,year,image}) {
   return (
     <CardContainer className="inter-var">
       <CardBody
-        className="bg-[#53565A] relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[20rem] h-[22rem] rounded-xl p-6 border"
+        className="bg-[#53565A] relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[24rem] h-[24rem] rounded-xl p-6 border"
       >
         <CardItem
           translateZ="50"
-          className="text-xl font-bold text-white"
+          className="text-xl font-bold text-white pt-4"
         >
           {title}
         </CardItem>
@@ -22,18 +22,20 @@ export function CardCom({title,description,live,code,image}) {
           className="text-sm max-w-sm mt-2 text-neutral-300"
         >
           {description}
+          <br />
+          {year}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <img
             src={image}
             height="1000"
             width="1000"
-            className="h-50 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            className="h-48 w-full object-cover rounded-xl group-hover/card:shadow-xl"
             alt="thumbnail"
           />
         </CardItem>
         <div className="flex justify-between items-center mt-8">
-          <CardItem
+          {/* <CardItem
             translateZ={20}
             className="px-4 py-2 rounded-xl text-xs font-normal text-white cursor-pointer"
           >
@@ -45,7 +47,7 @@ export function CardCom({title,description,live,code,image}) {
             className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
           >
             <a href={code} target="_blank">Code</a>
-          </CardItem>
+          </CardItem> */}
         </div>
       </CardBody>
     </CardContainer>
