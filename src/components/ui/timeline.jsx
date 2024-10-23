f902144e8a8a0319523fdcfa4cf400cc7e854cf4
TusharSin810 <tsinghal666@gmail.com>
2024-10-23 21:02:33 +0530

    Changes To EXP Section

--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -29,11 +29,11 @@ export const Timeline = ({
       className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}>
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-4xl mb-4 text-black dark:text-white max-w-4xl font-semibold">
+        <h2 className="text-lg md:text-5xl mb-4 text-black dark:text-white max-w-4xl font-semibold">
           Experience
         </h2>
         <p
-          className="text-neutral-700 dark:text-neutral-300 text-sm md:text-base max-w-sm">
+          className="text-neutral-700 dark:text-neutral-300 text-sm md:text-lg max-w-sm">
           I Am A 3rd Year Btech Student.
         </p>
       </div>
