3f76fbb628defdd71565a5d1f55a5d165b5db5a4
TusharSin810 <tsinghal666@gmail.com>
2024-10-28 17:47:24 +0530

    UI IMporovements

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,7 +6,7 @@ function Education() {
   return (
     <div className='bg-white h-max w-screen flex flex-col pt-10 gap-10 px-2 pb-4 sm:px-6'>
       <ScrollAnimation>
-       <h2 className='font-extrabold text-6xl text-[#4D0011] pb-4'>Education</h2>
+       <h2 className='font-extrabold text-6xl text-[#4D0011] pb-6'>Education</h2>
        <div className='w-full sm:flex sm:justify-evenly'>
         {eCard.map((data, index) => (
           <CardCom key={data.id || index} title={data.title} description={data.description} image={data.image} year={data.year}/>
--- src/components/ui/timeline.jsx
+++ src/components/ui/timeline.jsx
@@ -29,12 +29,12 @@ export const Timeline = ({
       className="w-full bg-white dark:bg-neutral-950 font-sans md:px-10"
       ref={containerRef}>
       <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
-        <h2 className="text-lg md:text-5xl mb-4 text-black dark:text-white max-w-4xl font-semibold">
+        <h2 className="text-lg md:text-5xl mb-4 text-[#4D0011] dark:text-white max-w-4xl font-semibold">
           Experience
         </h2>
         <p
           className="text-neutral-700 dark:text-neutral-300 text-sm md:text-lg max-w-sm">
-          I Am A 3rd Year Btech Student.
+          I Am A 3rd Year Btech Student. Currently Working On My Skills
         </p>
       </div>
       <div ref={ref} className="relative max-w-7xl mx-auto pb-20">
